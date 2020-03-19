903a1e643a5741c2a0a26207e9c5220cd3a5dd21
WayneUbuntu <1241401289@qq.com>
2020-03-19 19:13:08 +0800

    添加redisTemplate配置

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # redisTemplate
 
 使用redisTemplate的方式使用redis缓存
+    
+    1. 使用docker容器启动并运行redis
+    docker run -p 6379:6379 -d redis:latest redis-server
+    2. 连接docker容器中的redis服务时，需要知道redis对外暴露的ip
+    可使用 docker inspect NAMES 查看对应服务的IPAddress
+    3. 连接docker中启动的服务
+    docker exec -it NAMES redis-cli
--- src/test/java/com/example/demo/RedisDemoApplicationTests.java
+++ src/test/java/com/example/demo/RedisDemoApplicationTests.java
@@ -1,10 +1,39 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class RedisDemoApplicationTests {
+
+	@Autowired
+	StringRedisTemplate stringRedisTemplate; //操作 k-v 字符串
+
+	@Autowired
+	RedisTemplate redisTemplate;  //k- v 都是对象
+
+	/**
+	 * redis 常见
+	 * String(字符串) List(列表) Set(集合) Hash(散列) ZSet(有序集合)
+	 */
+
+	@Test
+	public void test1() {
+		stringRedisTemplate.opsForValue().append("StringKey", "字符串数值");
+		String value = stringRedisTemplate.opsForValue().get("StringKey");
+		System.out.println(value);
+	}
+
+	@Test
+	public void test2() {
+//
+//		Product product =  productMapper.getProductById(4L);
+//		redisTemplate.opsForValue().set("produxtid4",product);
+	}
+
 
 	@Test
 	void contextLoads() {
